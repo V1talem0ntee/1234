68b24be85c0345352d3795ce5b57424aff0c515e
V1talem0ntee <byuan1@sfsu.edu>
2025-07-22 22:38:48 -0700

    Milestone2: merge and prepare to push to main

--- application/app.js
+++ application/app.js
@@ -54,3 +54,8 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
--- application/routes/index.js
+++ application/routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"[Insert your name here]" });
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home", script: "index.js" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
+router.get("/register", (req, res) => {
+  res.render("register", { title: "Register", script: "Register.js" });
+});
+
+router.get("/postvideo", (req, res) => {
+  res.render("postvideo", { title: "Post Video" });
+});
+
+router.get("/viewpost", (req, res) => {
+  res.render("viewpost", { title: "View Video" });
 });
 
 module.exports = router;
