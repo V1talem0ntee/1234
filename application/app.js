88b89a02c852969562798b284223257bc379041e
V1talem0ntee <banghaoyuan@gmail.com>
2025-07-23 11:47:56 -0700

    Update app.js

--- application/app.js
+++ application/app.js
@@ -55,7 +55,3 @@ app.use(function (err, req, res, next) {
 
 module.exports = app;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`✅ Server is running on http://localhost:${PORT}`);
-});
